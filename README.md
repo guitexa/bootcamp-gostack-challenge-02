83f38cbb5e044b6957cd2edeb8e30d2aa6a7d8a9
Guilherme Teixeira <guilhermetexa@outlook.com>
2020-04-11 16:01:36 -0300

    Finish challenge

---
+++ README.md
@@ -0,0 +1,30 @@
+<p align="center">
+    <img alt="GoStack" src="https://ap.imagensbrasil.org/images/2020/04/09/banner-bootcamp-gostack-11.png" />
+</p>
+
+## :rocket: Desafio 02: Conceitos do Node.js
+
+[Acessar o desafio](https://github.com/Rocketseat/bootcamp-gostack-desafios/tree/master/desafio-conceitos-nodejs)
+
+🏁 Desafio completado em 11 de abril de 2020
+
+## ⚙️ Instalação
+
+```Bash
+# Instale as dependências deste projeto
+yarn
+
+# Execute a aplicação
+yarn dev
+```
+
+## 🚧 Informação
+
+> Essa aplicação não possui frontend, foi utilizado o Insomnia REST Client para fazer as requisições com o backend
+
+[![github](http://ap.imagensbrasil.org/images/2018/12/10/github-logo-1.png) ](https://github.com/guitexa)
+[![linkedin](http://ap.imagensbrasil.org/images/2018/12/10/linkedin-1.png)](https://www.linkedin.com/in/guitexa/)
+
+## Licença
+
+[MIT](./LICENSE) &copy; [Rocketseat](https://rocketseat.com.br/)
--- src/app.js
+++ src/app.js
@@ -1,33 +1,78 @@
-const express = require("express");
-const cors = require("cors");
+const express = require('express');
+const cors = require('cors');
 
-// const { uuid } = require("uuidv4");
+const { uuid } = require('uuidv4');
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
+app.use('/repositories/:id', (request, response, next) => {
+  const { id } = request.params;
+
+  const getIndex = repositories.findIndex((repository) => repository.id === id);
+
+  if (getIndex < 0) {
+    return response.status(400).json({ error: 'ID not found!' });
+  }
+
+  return next();
+});
+
 const repositories = [];
 
-app.get("/repositories", (request, response) => {
-  // TODO
+app.get('/repositories', (request, response) => {
+  return response.json(repositories);
 });
 
-app.post("/repositories", (request, response) => {
-  // TODO
+app.post('/repositories', (request, response) => {
+  const { title, url, techs } = request.body;
+
+  const repository = {
+    id: uuid(),
+    title,
+    url,
+    techs,
+    likes: 0,
+  };
+
+  repositories.push(repository);
+
+  return response.json(repository);
 });
 
-app.put("/repositories/:id", (request, response) => {
-  // TODO
+app.put('/repositories/:id', (request, response) => {
+  const { id } = request.params;
+  const { title, url, techs } = request.body;
+
+  const getIndex = repositories.findIndex((repository) => repository.id === id);
+
+  repositories[getIndex].title = title;
+  repositories[getIndex].url = url;
+  repositories[getIndex].techs = techs;
+
+  return response.json(repositories[getIndex]);
 });
 
-app.delete("/repositories/:id", (request, response) => {
-  // TODO
+app.delete('/repositories/:id', (request, response) => {
+  const { id } = request.params;
+
+  let getIndex = repositories.findIndex((repository) => repository.id === id);
+
+  repositories.splice(getIndex, 1);
+
+  return response.status(204).send();
 });
 
-app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+app.post('/repositories/:id/like', (request, response) => {
+  const { id } = request.params;
+
+  let getIndex = repositories.findIndex((repository) => repository.id === id);
+
+  repositories[getIndex].likes += 1;
+
+  return response.json(repositories[getIndex]);
 });
 
 module.exports = app;
